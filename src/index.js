ed5bf4e6bcbd5b8aaec2c910f8364a84e9f8f41d
Angel <50711051+SadJ0k3r@users.noreply.github.com>
2022-09-17 10:46:03 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//contentEditable="true" spellCheck="false">
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">Agricultural Currriculum</h1>
+
+    <img
+      className="agricultural-img"
+      src="https://static.vecteezy.com/system/resources/thumbnails/001/911/800/small/welder-oxygen-acetylene-welding-side-view-circle-vector.jpg"
+    />
+    <img
+      className="agricultural-img"
+      src="https://img.freepik.com/premium-vector/hydroponics-benefits-concept-with-water-saving-fast-growth-symbols-cartoon_1284-55527.jpg"
+    />
+    <img
+      className="agricultural-img"
+      src="https://static.vecteezy.com/system/resources/thumbnails/004/896/124/small/mound-plants-shovel-colorful-icon-plant-tree-agriculture-farming-gardening-concept-illustration-flat-design-eps10-vector.jpg"
+    />
+  </div>,
+  document.getElementById("root")
+);
